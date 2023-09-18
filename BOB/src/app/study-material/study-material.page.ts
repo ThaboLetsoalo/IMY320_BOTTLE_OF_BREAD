dd2319237937f40b3e316e0758c6fa978d234c74
Mduduzi Sibiya <91707808+Duzzii@users.noreply.github.com>
2023-09-18 12:15:03 +0200

    Few changes on my pages

--- BOB/src/app/study-material/study-material.page.ts
+++ BOB/src/app/study-material/study-material.page.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { StudyMaterialService } from './study-material.service';
 
 @Component({
   selector: 'app-study-material',
   templateUrl: './study-material.page.html',
   styleUrls: ['./study-material.page.scss'],
+  providers: [StudyMaterialService],
+
 })
 export class StudyMaterialPage implements OnInit {
+  categories: any[] = [];
+  selectedCategory: any;
+  studyMaterials: any[] = [];
 
-  constructor() { }
+  constructor(private studyMaterialService: StudyMaterialService) {}
 
   ngOnInit() {
+    this.fetchCategories();
   }
 
+  fetchCategories() {
+    this.studyMaterialService.getCategories().subscribe((data) => {
+      this.categories = data;
+    });
+  }
+
+  openCategory(categoryId: number) {
+    this.studyMaterialService.getStudyMaterialsByCategory(categoryId).subscribe((data: any) => {
+      // Update the selected category and study materials with the fetched data
+      this.selectedCategory = this.categories.find((category) => category.id === categoryId);
+      this.studyMaterials = data;
+    });
+  }
+  
 }
