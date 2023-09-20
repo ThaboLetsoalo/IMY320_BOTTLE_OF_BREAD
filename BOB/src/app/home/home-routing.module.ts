3c677ec3474627e29b97cdcf14ea63ad26709223
Thabo Letsoalo <126817800+ThaboLetsoalo@users.noreply.github.com>
2023-09-21 00:51:42 +0200

    routing modified

--- BOB/src/app/home/home-routing.module.ts
+++ BOB/src/app/home/home-routing.module.ts
@@ -6,7 +6,46 @@ import { HomePage } from './home.page';
 const routes: Routes = [
   {
     path: '',
-    component: HomePage
+    component: HomePage,
+    children: [
+      {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full'
+      },
+      {
+        path: 'dashboard',
+        loadChildren: () => import('../dashboard/dashboard.module').then( m => m.DashboardPageModule)
+      },
+      {
+        path: 'study-material',
+        loadChildren: () => import('../study-material/study-material.module').then( m => m.StudyMaterialPageModule)
+      },
+      {
+        path: 'schedules',
+        loadChildren: () => import('../schedules/schedules.module').then( m => m.SchedulesPageModule)
+      },
+      {
+        path: 'tutor-alumni',
+        loadChildren: () => import('../tutor-alumni/tutor-alumni.module').then( m => m.TutorAlumniPageModule)
+      },
+      {
+        path: 'about',
+        loadChildren: () => import('../about/about.module').then( m => m.AboutPageModule)
+      },
+      {
+        path: 'study-groups',
+        loadChildren: () => import('../study-groups/study-groups.module').then( m => m.StudyGroupsPageModule)
+      },
+      {
+        path: 'support-and-help',
+        loadChildren: () => import('../support-and-help/support-and-help.module').then( m => m.SupportAndHelpPageModule)
+      },
+      {
+        path: 'event-creation',
+        loadChildren: () => import('../event-creation/event-creation.module').then( m => m.EventCreationPageModule)
+      },
+    ]
   }
 ];
 
