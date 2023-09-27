a360f29ce37f1433169d2505b40d4cf4791a448b
UnarineLeo <unarineleo@gmail.com>
2023-09-27 06:50:55 +0200

    registration page

--- BOB/src/app/register/register.page.ts
+++ BOB/src/app/register/register.page.ts
@@ -16,6 +16,6 @@ export class RegisterPage {
   register() {
     // Implement your registration logic here.
     // For demonstration purposes, we'll navigate to a dummy success page.
-    this.router.navigate(['/registration-success']);
+    this.router.navigate(['//home/dashboard']);
   }
 }
