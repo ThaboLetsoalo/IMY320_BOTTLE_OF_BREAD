581753bd9f3e797aed14081213b807d4f541f1f0
Thabo Letsoalo <126817800+ThaboLetsoalo@users.noreply.github.com>
2023-10-14 18:21:17 +0200

    minor ui changes

--- BOB/src/app/ui/support-and-help/support-and-help.page.ts
+++ BOB/src/app/ui/support-and-help/support-and-help.page.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./support-and-help.page.scss'],
 })
 export class SupportAndHelpPage implements OnInit {
-
+  suggestedQuestions: string[] =["Question one", "Question 2", "How do I become a member"];
   constructor() { }
 
   ngOnInit() {
