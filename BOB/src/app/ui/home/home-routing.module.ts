6909453bfc08de63790b68600d92c60a229f96c9
Thabo Letsoalo <126817800+ThaboLetsoalo@users.noreply.github.com>
2023-10-15 13:11:01 +0200

    quick fix

--- BOB/src/app/ui/home/home-routing.module.ts
+++ BOB/src/app/ui/home/home-routing.module.ts
@@ -10,7 +10,7 @@ const routes: Routes = [
     children: [
       {
         path: '',
-        redirectTo: 'dashboard',
+        redirectTo: 'study-material',
         pathMatch: 'full'
       },
       {
