f4070139d7933f439ee6410690d3cc72f66f3453
UnarineLeo <unarineleo@gmail.com>
2023-10-15 05:22:28 +0200

    navigation update

--- BOB/src/app/app-routing.module.ts
+++ BOB/src/app/app-routing.module.ts
@@ -9,7 +9,7 @@ const routes: Routes = [
   },
   {
     path: 'home',
-    loadChildren: () => import('./ui/home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./ui/study-material/study-material.module').then( m => m.StudyMaterialPageModule)
   },
   {
     path: 'register',
